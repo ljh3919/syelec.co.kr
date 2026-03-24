3d59ae8ae3e6e2c240607a9f5359eb71b61f10dc
ljh3919 <ljh3919@gmail.com>
2026-03-24 17:29:01 +0900

    메인 페이지(index)에서만 우측 플로팅 배너 숨김 처리

--- theme/sy-elec/tail.php
+++ theme/sy-elec/tail.php
@@ -42,6 +42,7 @@ if (!defined('_INDEX_') && !$is_legacy_page) {
 </footer>
 
 <!-- Floating Buttons -->
+<?php if (!defined('_INDEX_')) { ?>
 <div class="floating-chat">
     <a href="https://pf.kakao.com/_xxxx" target="_blank" class="float-btn kakao">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path></svg>
@@ -52,6 +53,7 @@ if (!defined('_INDEX_') && !$is_legacy_page) {
         전화상담
     </a>
 </div>
+<?php } ?>
 
 <?php
 include_once(G5_PATH . '/tail.sub.php');
