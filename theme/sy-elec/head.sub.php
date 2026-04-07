37e0413cd5ef4b4793fd8e1e315d1f5fbd602070
ljh3919 <ljh3919@gmail.com>
2026-04-07 10:21:01 +0900

    .

--- theme/sy-elec/head.sub.php
+++ theme/sy-elec/head.sub.php
@@ -2,11 +2,7 @@
 // 이 파일은 새로운 파일 생성시 반드시 포함되어야 함
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-// 테마 head.sub.php 파일
-if(!defined('G5_IS_ADMIN') && defined('G5_THEME_PATH') && is_file(G5_THEME_PATH.'/head.sub.php')) {
-    require_once(G5_THEME_PATH.'/head.sub.php');
-    return;
-}
+// 테마 head.sub.php 파일이므로 자신의 경로를 다시 호출하지 않습니다.
 
 $g5_debug['php']['begin_time'] = $begin_time = get_microtime();
 
